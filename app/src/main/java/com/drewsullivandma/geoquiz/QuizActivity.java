bdec65f8034cf1c3d5c366b7ceaaba0587b84b04
dsullivan <drew.sullivan@hyland.com>
2018-06-13 16:52:46 -0400

    Committing lessons

--- app/src/main/java/com/drewsullivandma/geoquiz/QuizActivity.java
+++ app/src/main/java/com/drewsullivandma/geoquiz/QuizActivity.java
@@ -1,5 +1,6 @@
 package com.drewsullivandma.geoquiz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -17,6 +18,7 @@ public class QuizActivity extends AppCompatActivity {
     private Button mFalseButton;
     private ImageButton mNextButton;
     private ImageButton mPreviousButton;
+    private Button mCheatButton;
     private TextView mQuestionTextView;
     private Question[] mQuestionBank = new Question[] {
             new Question(R.string.question_math, true),
@@ -26,6 +28,8 @@ public class QuizActivity extends AppCompatActivity {
             new Question(R.string.question_politics, false)
     };
     private int mCurrentIndex = 0;
+    private Boolean[] answerTally = new Boolean[mQuestionBank.length];
+    private int numCorrectAnswers = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,12 +40,12 @@ public class QuizActivity extends AppCompatActivity {
             mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
         }
 
-
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
         mTrueButton = (Button) findViewById(R.id.true_button);
         mFalseButton = (Button) findViewById(R.id.false_button);
         mNextButton = (ImageButton) findViewById(R.id.next_button);
         mPreviousButton = (ImageButton) findViewById(R.id.previous_button);
+        mCheatButton = (Button) findViewById(R.id.cheat_button);
 
         mQuestionTextView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -75,6 +79,14 @@ public class QuizActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 moveToNextQuestion();
+            }
+        });
+
+        mCheatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(QuizActivity.this, CheatActivity.class);
+                startActivity(intent);
             }
         });
         updateQuestion();
@@ -120,17 +132,35 @@ public class QuizActivity extends AppCompatActivity {
     private void updateQuestion() {
         int question = mQuestionBank[mCurrentIndex].getTextResId();
         mQuestionTextView.setText(question);
+        if (answerTally[mCurrentIndex] == null) {
+            enableAnswerButtons();
+        }
     }
 
     private void checkAnswer(boolean userPressedTrue) {
+        if (answerTally[mCurrentIndex] != null) {
+            disableAnswerButtons();
+        }
         boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
         int messageResId = 0;
         if (userPressedTrue == answerIsTrue) {
+            numCorrectAnswers++;
+            answerTally[mCurrentIndex] = true;
             messageResId = R.string.correct_toast;
         } else {
+            answerTally[mCurrentIndex] = false;
             messageResId = R.string.incorrect_toast;
         }
-        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
+        disableAnswerButtons();
+
+        if (continueQuiz() == true) {
+            Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
+        } else {
+            String completionMessage = "You answered " + numCorrectAnswers + "/" + mQuestionBank.length + " right!";
+            Toast.makeText(this, completionMessage, Toast.LENGTH_LONG).show();
+            enableAnswerButtons();
+        }
+
     }
 
     private void updateCurrentIndex() {
@@ -145,5 +175,41 @@ public class QuizActivity extends AppCompatActivity {
     private void moveToPreviousQuestion() {
         mCurrentIndex = (mCurrentIndex + mQuestionBank.length - 1) % mQuestionBank.length;
         updateQuestion();
+    }
+
+    private void enableAnswerButtons() {
+        mTrueButton.setEnabled(true);
+        mFalseButton.setEnabled(true);
+    }
+
+    private void disableAnswerButtons() {
+        mTrueButton.setEnabled(false);
+        mFalseButton.setEnabled(false);
+    }
+
+    private boolean continueQuiz() {
+        for (Boolean answer : answerTally) {
+            if (answer == null) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void checkTally() {
+        if (getNumAnswersRemaining() == 0) {
+            String completionMessage = "You answered " + (Math.round((double) numCorrectAnswers / mQuestionBank.length)) + "% right!";
+            Toast.makeText(this, completionMessage, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private int getNumAnswersRemaining() {
+        int numAnswersRemaining = 0;
+        for (Boolean answer : answerTally) {
+            if (answer == null) {
+                numAnswersRemaining++;
+            }
+        }
+        return numAnswersRemaining;
     }
 }
